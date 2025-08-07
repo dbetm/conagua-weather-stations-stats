4a4fed02882252815cc1f5709ad515324308971d
David Betancourt <davbetm@gmail.com>
2025-08-06 21:44:35 -0600

    custom title when ploting max temperatures

--- ploting.py
+++ ploting.py
@@ -6,28 +6,52 @@ from contants import MONTH_TO_MONTH_NAME
 
 
 def plot_max_temperatures(
-    max_temp_df: pd.DataFrame, station_name: str, days_per_period: int, extra_x_markers: dict
+    max_temp_df: pd.DataFrame,
+    station_name: str,
+    days_per_period: int,
+    extra_x_markers: dict,
+    show: bool = True,
+    title: Optional[str] = None,
+    save_path: Optional[str] = None,
 ):
     plt.figure(figsize=(12, 6))
 
-    # Itera sobre cada columna (cada año) y grafica su serie
+    # compute daily average max temperature
+    daily_avg = max_temp_df.mean(axis=1)
+
+    # Plot daily average of max temperature
+    plt.plot(max_temp_df.index, daily_avg, label="Promedio", color="black")
+
+    # Per year plot their serie
     for year in max_temp_df.columns:
         plt.plot(max_temp_df.index, max_temp_df[year], label=str(year))
 
     plt.xticks(ticks=range(0, len(max_temp_df), 10))
 
-    # Añadir etiquetas de meses clave
+    # Add labels by month
     for marker, pos in extra_x_markers.items():
         plt.text(pos, plt.ylim()[0] - 1, marker, ha='center', va='top')
+        #plt.text(pos, 13, marker, ha='center', va='top')
 
-    # Opciones de la gráfica
-    plt.title(f"Temperaturas máximas diarias en la estación {station_name} (por año)")
-    plt.xlabel("Día (0-91)")
+    # Setup plot
+    if title:
+        plt.title(f"{title} - Estación: {station_name}")
+    else:
+        plt.title(f"Temperaturas máximas diarias en la estación {station_name} (por año)")
+    plt.xlabel(f"Día (1-{days_per_period})")
     plt.ylabel("Temperatura máxima (°C)")
     plt.legend(title="Año")
     plt.grid(True)
+
+    #plt.ylim(bottom=15, top=45)
+    #plt.yticks([15, 25, 30, 35, 40, 45])
     plt.tight_layout()
-    plt.show()
+
+    if show:
+        plt.show()
+
+    if save_path:
+        plt.savefig(save_path, dpi=300, bbox_inches='tight')
 
 
 def plot_accumulated_precipitation(
